42b8e6fadcfbb20c98f52fe916981db4ff03948e
Emon Hossain <emondd4@gmail.com>
2022-05-16 22:18:02 +0600

    notification flow bump 1

--- lib/ui/ninja/NotificationScreen.dart
+++ lib/ui/ninja/NotificationScreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tokenapp/ui/ninja/ClaimRewardScreen.dart';
+import 'package:tokenapp/ui/ninja/DailyRewardScreen.dart';
+import 'package:tokenapp/ui/ninja/RewardScreen.dart';
 import '../../Utils/AppCustomColors.dart';
 
 class NotificationPage extends StatefulWidget {
@@ -63,96 +66,108 @@ class _NotificationPageState extends State<NotificationPage> {
   Widget _listItem(){
     return Padding(
       padding: const EdgeInsets.fromLTRB(15.0,10.0,15.0,10.0),
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: Colors.white
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    Container(
-                      height: 48.0,
-                      width: 48.0,
-                      decoration: BoxDecoration(
-                          color: AppCustomColors.lightCreamBlue,
-                          borderRadius: BorderRadius.circular(50.0)
+      child: InkWell(
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 48.0,
+                        width: 48.0,
+                        decoration: BoxDecoration(
+                            color: AppCustomColors.lightCreamBlue,
+                            borderRadius: BorderRadius.circular(50.0)
+                        ),
+                        child: Icon(Icons.flag_rounded, color: Colors.white,),
                       ),
-                      child: Icon(Icons.flag_rounded, color: Colors.white,),
-                    ),
-                    const SizedBox(width: 8.0,),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Text(
-                                "Daily Reward",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 18.0,
-                                    color: AppCustomColors.textDarkColor),
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(width: 5.0,),
-                              Container(
-                                width: MediaQuery.of(context).size.width / 6,
-                                height: 25.0,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(25.0),
-                                    color: AppCustomColors.pageStartColor),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SvgPicture.asset("assets/images/ic_coin.svg"),
-                                    const SizedBox(
-                                      width: 3.0,
-                                    ),
-                                    const Text(
-                                      "+50",
-                                      style: TextStyle(
-                                          fontSize: 15.0,
-                                          fontWeight: FontWeight.bold,
-                                          color: AppCustomColors.textCoinColor),
-                                    )
-                                  ],
+                      const SizedBox(width: 8.0,),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Text(
+                                  "Daily Reward",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 18.0,
+                                      color: AppCustomColors.textDarkColor),
+                                  textAlign: TextAlign.center,
                                 ),
-                              ),
-                            ],
-                          ),
-                          const Text(
-                            "Collaboratively harness high profile",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 15.0,
-                                color: AppCustomColors.textLightColor),
-                            textAlign: TextAlign.start,
-                          )
-                        ],
+                                const SizedBox(width: 5.0,),
+                                Container(
+                                  width: MediaQuery.of(context).size.width / 6,
+                                  height: 25.0,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(25.0),
+                                      color: AppCustomColors.pageStartColor),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SvgPicture.asset("assets/images/ic_coin.svg"),
+                                      const SizedBox(
+                                        width: 3.0,
+                                      ),
+                                      const Text(
+                                        "+50",
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            fontWeight: FontWeight.bold,
+                                            color: AppCustomColors.textCoinColor),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Text(
+                              "Collaboratively harness high profile",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15.0,
+                                  color: AppCustomColors.textLightColor),
+                              textAlign: TextAlign.start,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(width: 8.0,),
-              const Text(
-                "2 days ago",
-                style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 13.0,
-                    color: Colors.black26),
-              ),
-            ],
+                const SizedBox(width: 8.0,),
+                const Text(
+                  "2 days ago",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13.0,
+                      color: Colors.black26),
+                ),
+              ],
+            ),
           ),
         ),
+        onTap: () {
+          showDialog(context: context, builder: (context) { return _showDialog();});
+        },
       ),
+    );
+  }
+
+  Widget _showDialog(){
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+      child: RewardPage(),
     );
   }
 
--- lib/ui/ninja/RewardScreen.dart
+++ lib/ui/ninja/RewardScreen.dart
@@ -12,77 +12,75 @@ class RewardPage extends StatefulWidget {
 class _RewardPageState extends State<RewardPage> {
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.55,
-        width: MediaQuery.of(context).size.width * 0.50,
-        decoration: BoxDecoration(
-            color: Colors.transparent,
-            borderRadius: BorderRadius.circular(5.0),
-            border: Border.all(color: Colors.red, width: 1.0)),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10.0,
+    return Container(
+      width: 300,
+      height: 320,
+      decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(5.0),
+          border: Border.all(color: Colors.red, width: 1.0)),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10.0,
+          ),
+          Image.asset("images/crown.png"),
+          const Text(
+            "Daily Reward",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+                color: Colors.black),
+          ),
+          const SizedBox(height: 5.0,),
+          Container(
+            width: MediaQuery.of(context).size.width / 5,
+            height: 25.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: AppCustomColors.pageStartColor),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset("assets/images/ic_coin.svg"),
+                const SizedBox(
+                  width: 5.0,
+                ),
+                const Text(
+                  "+50",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: AppCustomColors.textCoinColor),
+                )
+              ],
             ),
-            Image.asset("assets/images/crown.png"),
-            const Text(
-              "Daily Reward",
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0,
-                  color: Colors.black),
-            ),
-            const SizedBox(height: 5.0,),
-            Container(
-              width: MediaQuery.of(context).size.width / 5,
-              height: 25.0,
+          ),
+          const SizedBox(height: 10,),
+          const Text("Uniquely Communicate Real-Time", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black),),
+          const Text("\"Outside the Box\" Thinking", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black),),
+          const SizedBox(height: 30.0,),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.5,
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25.0),
-                  color: AppCustomColors.pageStartColor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset("assets/images/ic_coin.svg"),
-                  const SizedBox(
-                    width: 5.0,
-                  ),
-                  const Text(
-                    "+50",
-                    style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: AppCustomColors.textCoinColor),
-                  )
-                ],
+                borderRadius: BorderRadius.circular(25.0),
+                gradient: const LinearGradient(
+                  begin: Alignment(-1.0, -2.0),
+                  end: Alignment(1.0, 2.0),
+                  colors: <Color>[
+                    AppCustomColors.buttonStartColor,
+                    AppCustomColors.buttonEndColor
+                  ],
+                ),
+              ),
+              child: const Padding(padding: EdgeInsets.all(15.0), child: Text("Claim Reward", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0, color: Colors.white), textAlign: TextAlign.center,)
               ),
             ),
-            const SizedBox(height: 10,),
-            const Text("Uniquely Communicate Real-Time", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black),),
-            const Text("\"Outside the Box\" Thinking", style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.normal, color: Colors.black),),
-            const SizedBox(height: 30.0,),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 15.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width / 1.5,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25.0),
-                  gradient: const LinearGradient(
-                    begin: Alignment(-1.0, -2.0),
-                    end: Alignment(1.0, 2.0),
-                    colors: <Color>[
-                      AppCustomColors.buttonStartColor,
-                      AppCustomColors.buttonEndColor
-                    ],
-                  ),
-                ),
-                child: const Padding(padding: EdgeInsets.all(15.0), child: Text("Claim Reward", style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0, color: Colors.white), textAlign: TextAlign.center,)
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
